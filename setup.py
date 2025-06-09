f23a5a1bc0397a2a2cd4bc1b276056297be1cf78
magno204 <ceps32@hotmail.com>
2025-06-09 11:00:55 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cuartoRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción 
+    author="Carlos Pineda",                         # Tu nombre
+    author_email="carlos.pineda@adenion.com.co",    # Tu correo electrónico
+    url="https://github.com/magno204/cuartoRepo",   # URL del proyecto
+)
